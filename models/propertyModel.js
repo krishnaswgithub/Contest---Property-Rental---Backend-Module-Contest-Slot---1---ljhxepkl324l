49b677af044d4da770255c34dd1ac6f0b59fc52b
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-12-30 19:20:36 +0530

    Update propertyModel.js

--- models/propertyModel.js
+++ models/propertyModel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const propertySchema = new mongoose.Schema({
-  // TODO: Define the properties of the model
-  // Field 1: Define a property for 'title' with a String data type and required true
-  // Field 2: Define a property for 'location' with a String data type and required true
-  // Field 3: Define a property for 'price' with a Number data type and required true
-  // Field 4: Define a property for 'type' with a String data type and required true
+  title: { type: String, required: true },
+  location: { type: String, required: true },
+  price: { type: Number, required: true },
+  type: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Property', propertySchema);
